5dbcb555eae8a7d040d43d0205df2c5a9312601a
Huib Keemink <huib.keemink@creativedutchmen.com>
2017-06-02 13:11:26 +0200

    proposed changes for day of week

--- hackaton_eet/data/features.py
+++ hackaton_eet/data/features.py
@@ -4,8 +4,7 @@ Placeholder for all feature engineering
 
 def add_day_as_feature(feature_data):
     # add day index
-    daydict = {'Monday': 0, 'Tuesday': 1, 'Wednesday': 2, 'Thursday': 3, 'Friday': 4, 'Saturday': 5, 'Sunday': 7}
-    dayidx = [daydict[day] for day in feature_data.T.index.weekday_name]
+    dayidx = feature_data.T.index.weekday_name
 
     feature_data = feature_data.T
     feature_data['day_idx'] = dayidx
