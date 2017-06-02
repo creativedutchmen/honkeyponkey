5e3f6ed84a07321ac8916e98870f475a1ebb7003
Cees Taal <cees.taal@quby.com>
2017-06-02 13:09:55 +0200

    add ptu as new feature

--- hackaton_eet/data/features.py
+++ hackaton_eet/data/features.py
@@ -12,3 +12,13 @@ def add_day_as_feature(feature_data):
     feature_data = feature_data.T
 
     return feature_data
+
+def add_ptu_as_feature(feature_data):
+    ptu = feature_data.T.index.hour * 4 + (1 + feature_data.T.index.minute / 15)
+
+    feature_data = feature_data.T
+    feature_data['ptu'] = ptu
+    feature_data = feature_data.T
+
+    return feature_data
+
